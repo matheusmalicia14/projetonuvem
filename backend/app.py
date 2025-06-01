7507ca013aec0f3e7acd7bad2479acc5f79942c9
matheusmalicia14 <10417969@mackenzista.com.br>
2025-06-01 20:19:20 +0000

    commit

--- backend/app.py
+++ backend/app.py
@@ -41,5 +41,5 @@ def deletar_filme(id):
     filmes = [f for f in filmes if f["id"] != id]
     return jsonify({"mensagem": "Filme deletado"})
 
-if _name_ == '_main_':
+if __name__ == '_main_':
     app.run(host='0.0.0.0', port=5000)
